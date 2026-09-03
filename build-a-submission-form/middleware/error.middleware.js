091c9216bda69afc27aa43a6ebcacf3170e36d32
delm83 <delm83@googlemail.com>
2026-09-03 08:54:01 +0000

    completed build a submissioin form

---
+++ build-a-submission-form/middleware/error.middleware.js
@@ -0,0 +1,16 @@
+const notFoundHandler = (req, res, next) => {
+  const error = new Error(`Cannot find ${req.originalUrl}`);
+  error.status = 404;
+  next(error);
+}
+
+const finalErrorHandler = (err, req, res, next) => {
+  const status = err.status || 500;
+  res.status(status).json({
+      error: true,
+      status: status,
+      message: err.status===500?'Internal Server Error (Check Server Logs)':err.message
+    });
+}
+
+export { notFoundHandler, finalErrorHandler };
